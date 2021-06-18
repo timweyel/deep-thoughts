f423487bf44f71183ba9efad064e15650b51ea90
TimWeyel <tweyel@gmail.com>
2021-06-17 17:00:00 -0700

    added thought form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,12 +4,15 @@ import Auth from '../utils/auth';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Profile = () => {
   const { username: userParam } = useParams();
-
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
@@ -31,13 +34,28 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
       <h2 className="bg-dark text-secondary p-3 display-inline-block">
         Viewing {userParam ? `${user.username}'s` : 'your'} profile.
       </h2>
-      </div>
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+      )}
+    </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -52,6 +70,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
